30399027ed52f63b286af89b6f1d4e696bb878db
Neel Redkar <neel.redkar@gmail.com>
2020-08-26 00:26:31 -0700

    design changes

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,20 @@ export default ({ Component, props }) => {
             <Head>
                 <title>ensio</title>
                 <link rel="icon" href="/static/logo.png" />
+                <meta name="title" content="ensio" />
+                <meta name="description" content="Two dimensional video calling" />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://ensio.now.sh" />
+                <meta property="og:title" content="ensio" />
+                <meta property="og:description" content="Two dimensional video calling" />
+                <meta property="og:image" content="https://ensio.now.sh/static/front.png" />
+
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://ensio.now.sh" />
+                <meta property="twitter:title" content="ensio" />
+                <meta property="twitter:description" content="Two dimensional video calling" />
+                <meta property="twitter:image" content="https://ensio.now.sh/static/front.png" />
             </Head>
             <Flex flexDirection="column" minHeight="100vh">
                 <Nav />
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default () => {
     const [colorMode, setColorMode] = useColorMode()
     const email = useRef('')
     return (
-        <Flex w="100vw" pt="100px" flexDirection="column">
+        <Flex w="100vw" pt={["50px", "100px", "100px"]} flexDirection="column">
             <Heading m="auto" fontSize={[7, 7, 8]} sx={{
                 "-webkit-background-clip": "text",
                 "backgroundClip": "text !important",
@@ -16,7 +16,7 @@ export default () => {
                 "background": colorMode == "light" ? "linear-gradient(to right,#f5ecdb,#000)" : "linear-gradient(to right,#fff, #d3ba98)"
             }}>ensio.</Heading>
             <Text mx="auto" fontWeight="bold">Two dimensional video calling</Text>
-            <Text width={["90vw", "40vw", "40vw"]} mx="auto" sx={{ textIndent: "30px", py: "10px" }}>Video platforms like Zoom, or Google Meet are a pain for students and offices, because of the constant speaking over each other, and the ability to move aside and talk to another person seamlessly. Meet ensio, where you're able to drag and drop markers to create a board of video callers, where sound diffuses in a 2d plane just like in real life. With features like adjusable sound barriers to create rooms and sharable whiteboards, you are able to emulate a work or school enviorment with ease. <Text as="span" color="highlight" fontWeight="bold">Sign up for the alpha today!</Text></Text>
+            <Text width={["90vw", "40vw", "40vw"]} mx="auto" sx={{ py: "10px" }}>Video platforms like Zoom, or Google Meet are a pain for students and offices, because of the constant speaking over each other, and the inability to move across a 2nd dimenstion and talk to your friend individually. Meet ensio, where you're able to drag and drop markers to create a board of video callers, where sound diffuses in a 2d plane just like in real life. With features like adjusable sound barriers to create soundproof rooms, and sharable whiteboard collaboration, you are able to emulate a work or school enviorment with ease! <Text as="span" color="highlight" fontWeight="bold">Sign up for the alpha today!</Text></Text>
             <Flex mx="auto" sx={{
                 width: ["90vw", "35vw", "35vw"],
                 flexDirection: ["column", "row", "row"]
