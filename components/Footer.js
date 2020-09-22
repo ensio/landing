b41adbd4bd89e8a8f0615a4d542e6d1261251970
Neel Redkar <neel.redkar@gmail.com>
2020-09-21 18:59:55 -0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -12,5 +12,5 @@ let FootLink = ({ sx, ...props }) =>
 
 export default () =>
     <Flex mt="auto" height="100px">
-        <Text m="auto"><FootLink href="mailto:neel.redkar@gmail.com">Contact Me</FootLink> | <FootLink href="https://github.com/ensio-app">GitHub</FootLink></Text>
+        <Text m="auto"><FootLink href="mailto:contact@ensio.live">Contact Me</FootLink> | <FootLink href="https://github.com/ensio-app">GitHub</FootLink></Text>
     </Flex>
