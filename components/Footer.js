2bc62cb6c00be8e563d4d5745fd8e62e849732d1
Neel Redkar <neel.redkar@gmail.com>
2020-10-09 11:06:36 -0700

    add twiter

--- components/Footer.js
+++ components/Footer.js
@@ -12,5 +12,5 @@ let FootLink = ({ sx, ...props }) =>
 
 export default () =>
     <Flex mt="auto" height="100px">
-        <Text m="auto"><FootLink href="mailto:contact@ensio.live">Contact Me</FootLink> | <FootLink href="https://github.com/ensio">GitHub</FootLink></Text>
+        <Text m="auto"><FootLink href="mailto:contact@ensio.live">Contact Me</FootLink> | <FootLink href="https://github.com/ensio">GitHub</FootLink> | <FootLink href="https://twitter.com/ensio_live">Twitter</FootLink></Text>
     </Flex>
